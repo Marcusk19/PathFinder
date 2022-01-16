9621851530f4c4f183c09010c729e469e7c2da91
Marcus Kok <marcusk1699@gmail.com>
2022-01-16 11:06:51 -0500

    [SETUP] first commit

---
+++ src/HUD.py
@@ -0,0 +1,8 @@
+# Main code for HUD Display
+# Marcus Kok 1/16/22
+
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" # hide welcome prompt from pygame module
+import pygame
+
+print('Hello World')
