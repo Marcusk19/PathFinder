4aa0e143a6a694843a5e06aae5d79763c5cbdff1
Marcus Kok <marcusk1699@gmail.com>
2022-02-14 10:56:03 -0500

    mqtt handling in HUD.py

--- src/HUD.py
+++ src/HUD.py
@@ -3,18 +3,65 @@
 # Marcus Kok 1/16/22
 # [TODO] setup communication between app and phone
 
+# imports
+from ast import While
 import os, sys, getopt
 from directions import DirectionController
+import paho.mqtt.client as mqtt
+
+inputA = ""
+inputB = ""
+client = mqtt.Client() # instance of Client object
 
 def main():
-    # instance of directionController
-    input_A = sys.argv[1]
-    input_B = sys.argv[2]
+    MQTT_SERVER = "45.56.117.102" # ip address of linode instance
 
-    locA = str(input_A).replace(' ', '&')
-    locB = str(input_B).replace(' ', '&')
+    
+    client.on_connect = on_connect # override methods
+    client.on_message = on_message
 
-    print("Directions from " + str(input_A) + " to " + str(input_B))
+    client.connect(MQTT_SERVER) # connect to broker
+    client.loop_start() # start new thread loop to handle network
+    print("Waiting on user input...")
+
+    while True:
+        if inputA != "" and inputB != "":
+            break;
+
+    pull_directions()
+    client.loop_stop()
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result: " + str(rc))
+    client.subscribe([("source", 0), ("destination", 0)])
+    client.message_callback_add("source", on_source)
+    client.message_callback_add("destination", on_dest)
+
+
+def on_message(client, userdata, msg):
+    print("msg topic: " + str(msg.payload))
+
+# mqtt msg.payload comes in as b'message'
+def on_source(client, userdata, msg):
+    global inputA, inputB
+    inputA = str(msg.payload)
+    inputA = inputA[2 : len(inputA)-1] # get rid of mqtt formatting
+    print("inputA = " + inputA)
+
+def on_dest(client, userdata, msg):
+    global inputB, inputA
+    inputB = str(msg.payload)
+    inputB = inputB[2 : len(inputB)-1] # get rid of mqtt formatting
+    print("inputB = " + inputB)
+
+
+def pull_directions():
+        # massage inputs (spaces replaced with &)
+    locA = str(inputA).replace(' ', '&')
+    locB = str(inputB).replace(' ', '&')
+
+    print("Directions from " + str(inputA) + " to " + str(inputB))
 
     # initialize instance of DirectionController from directions.py
     controller = DirectionController(locA, locB)
@@ -30,6 +77,7 @@ def main():
     # print out instruction by instruction
     for instruction in instructions:
         print(instruction)
+    
 
 main()
 
--- src/mqtt_demo/mqtt_publisher.py
+++ src/mqtt_demo/mqtt_publisher.py
@@ -2,7 +2,8 @@
 
 import paho.mqtt.publish as publish
 
-MQTT_SERVER = "192.168.0.239"
+MQTT_SERVER = "45.56.117.102" # ip address of linode instance
 MQTT_PATH = "test_channel"
 
 publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
+print("Published message to " + MQTT_SERVER)   
