188bfbdd1a0c57eecc83d9b012e6104a9d72f716
Marcus Kok <marcusk1699@gmail.com>
2022-04-11 10:46:52 -0400

    😎 minor tweak in main

--- pathfinder/main.py
+++ pathfinder/main.py
@@ -1,7 +1,11 @@
 import HUD
 import OBD
 
-obd = OBD.Obd()
-obd.start()
-hud = HUD.Hud()
-hud.start()
+def main():
+    obd = OBD.Obd()
+    obd.start()
+    hud = HUD.Hud()
+    hud.start()
+
+if __name__ == "__main__":
+    main()
